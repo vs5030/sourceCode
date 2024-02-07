5f30bc39c56823f0c85d22b21c53dcab11d3df04
Vaibhav Sheth <vaibhavsheth@Vaibhavs-MacBook-Air.local>
2024-02-07 14:31:56 -0500

    commit search updates

--- wsearch/src/app/app.module.ts
+++ wsearch/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PageListComponent } from './page-list/page-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { PageListComponent } from './page-list/page-list.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
