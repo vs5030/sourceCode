0a306ef8da3e72988b8bc20fd3da5e7af6f730cb
Vaibhav Sheth <vaibhavsheth@Vaibhavs-MacBook-Air.local>
2024-02-21 19:43:20 -0500

    commit form changes

---
+++ creditcard/src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+  @Input() name!: string;
+  @Input() cardNumber!: string;
+  @Input() expiration!: string;
+  
+}
